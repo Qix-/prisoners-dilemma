2797ce1d28e24a8c12e2c9e4a6a9a58f20a8b163
Josh Junon <i.am.qix@gmail.com>
2015-12-16 06:36:48 -0700

    added strategy struct documentation

--- strategy.h
+++ strategy.h
@@ -3,8 +3,17 @@
 #pragma once
 
 typedef struct {
+	/* initialize some sort of "memory" construct, or 0 */
+	/* non-zero return values are automatically free()'d by the harness,
+	   so many sure you return an actual malloc()'d pointer! */
 	void * (*init)();
+
+	/* whether or not to cheat, given the current memory. */
+	/* 1 = confess (cheat), 0 = stay silent */
 	int (*cheat)(void *);
+
+	/* recieves whether or not the other prisoner confessed. */
+	/* this is after mixed signals have been applied, if any */
 	void (*report)(void *, int);
 } Strategy;
 
